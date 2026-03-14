23940595d8ea08ca684e2e6ef1fdfcacb3ace4d1
Autumn <Work@mail.autumnogborn.com>
2026-03-13 22:28:33 -0600

    Add Node and BinaryTree classes

---
+++ TreeApp/Models/BinaryTree.cs
@@ -0,0 +1,15 @@
+public class BinaryTree{
+  private Node? rootNode;
+  void Insert(int value){
+
+  }
+  string InOrder(){
+    return "";
+  }
+  int Height(){
+    return -1;
+  }
+  string ToMermaid(){
+    return "";
+  }
+}
---
+++ TreeApp/Models/Node.cs
@@ -0,0 +1,5 @@
+public class Node{
+  public int Value{get; private set;}
+  public Node? Left{get; set;} 
+  public Node? Right{get; set;} 
+}
